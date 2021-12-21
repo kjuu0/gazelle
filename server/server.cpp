5555499df6b4a70213f12f0f67c05ac7528eaabb
Brian Qiu <brianlqiu5@gmail.com>
2021-12-20 21:04:03 -0800

    basic rpc client-server communication with AWS EC2

--- server/server.cpp
+++ server/server.cpp
@@ -1,1 +1,17 @@
-int main() { return 0; }
+#include <iostream>
+#include "rpc/server.h"
+
+double add(double a, double b) { return a + b; }
+
+int main(int argc, char** argv) { 
+    if (argc < 2) {
+        std::cout << "Missing port" << std::endl;
+        exit(1);
+    }
+    
+    const int PORT = std::stoi(argv[1]);
+    rpc::server srv(PORT); 
+    srv.bind("add", &add);
+    srv.run();
+    return 0;
+}
