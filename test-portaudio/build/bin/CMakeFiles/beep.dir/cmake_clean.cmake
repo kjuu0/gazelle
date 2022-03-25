e1e9950a7b592db6aa88c1a8643c51a68a42e72f
Jerry Wang <tcjwang@outlook.com>
2022-03-25 13:36:59 -0700

    add paex_read_write_wire and backup

---
+++ test-portaudio/build/bin/CMakeFiles/beep.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/beep.dir/beep.c.o"
+  "CMakeFiles/beep.dir/beep.c.o.d"
+  "beep"
+  "beep.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/beep.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
