80ff1009f574ae78bf733eaa89d5908b8f1af211
Jerry Wang <tcjwang@outlook.com>
2022-05-09 15:12:11 -0700

    implement online dialing

---
+++ online_audio_process/env_setup.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get -y install g++
+sudo apt-get -y install libssl-dev
+sudo apt-get install -y build-essential autoconf libtool pkg-config
+
+# gazelle project
+sudo apt-get install git
+sudo apt-get install libasound-dev
+sudo apt-get install libtool
+sudo apt-get libtool-bin
+
+pushd ~
+wget pa_stable_v190700_20210406.tgz
+tar -xzvf pa_stable_v190700_20210406.tgz
+pushd portaudio
+./configure && make
+popd
+
+git clone https://github.com/xiph/LPCNet.git
+pushd LPCNet
+./autogen.sh
+./configure
+make
+export CFLAGS='-Ofast -g -march=native'
+popd
+
+# sudo apt-get install unzip
+# wget https://github.com/missmiss20/gazelle/archive/refs/heads/master.zip
+# unzip master.zip
+# gazelle project end
+
+wget https://github.com/Kitware/CMake/releases/download/v3.18.4/cmake-3.18.4.tar.gz
+tar -xzvf cmake-3.18.4.tar.gz
+pushd cmake-3.18.4
+./bootstrap
+./bootstrap
+make -j4
+sudo make install
+sudo ln -s /usr/local/bin/cmake /usr/bin/cmake
+popd
+sudo apt-get -y install clang
+
+git clone https://github.com/microsoft/SEAL.git
+pushd SEAL
+git checkout 3.5.6
+git pull
+cmake . -DBUILD_SHARED_LIBS=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+make
+sudo make install
+popd
+
+git clone https://github.com/rpclib/rpclib.git
+pushd rpclib
+cmake .
+make
+sudo make install
+popd
+
+popd
---
+++ online_audio_process/src/common/globals.h
@@ -0,0 +1,72 @@
+#ifndef ADDRA_GLOBAL_H
+#define ADDRA_GLOBAL_H
+
+
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include<unistd.h>
+#include<strings.h>
+#include<string.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include<vector>
+#include <pthread.h>
+#include <cstddef>
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <random>
+#include <thread>
+#include <mutex>
+#include <memory>
+#include <limits>
+#include <sstream> 
+#include<cmath>
+#include<ctime>
+#include<stack>
+
+#include <openssl/aes.h>
+#include <rpc/client.h>
+#include<rpc/server.h>
+#include "seal/seal.h"
+
+// portaudio and lpcnet added
+#include <stdio.h>
+#include <fstream>
+#include <portaudio.h>
+#include <lpcnet.h>
+
+// portaudio settings
+#define SAMPLE_RATE  (16000)
+#define FRAMES_PER_BUFFER (0)
+#define NUM_SECONDS     (2)
+#define NUM_CHANNELS    (1)
+#define PA_SAMPLE_TYPE  paInt16
+typedef short SAMPLE;
+#define SAMPLE_SILENCE  (0)
+#define PRINTF_S_FORMAT "%d"
+
+typedef struct
+{
+    int          frameIndex;  /* Index into sample array. */
+    int          maxFrameIndex;
+    SAMPLE*      recordedSamples; // float recorded samples
+}
+paTestData;
+// end of portaudio settings
+// end
+
+#define N 4096
+#define PLAIN_BIT 19
+#define COEFF_MODULUS_54 18014398509309953UL
+#define COEFF_MODULUS_55 36028797018652673UL
+#define PLAIN_MODULUS 270337
+#define NUM_CT_PER_QUERY (2 * (NUM_MESSAGE / N))
+#define SUBROUND_TIME 480 // In miliseconds
+#define RAW_DB_SIZE (MESSAGE_SIZE * NUM_MESSAGE)
+#define SERVER_PACKET_SIZE (8 * 1024)
+#define CLIENT_PACKET_SIZE (64 * 1024)
+#define CT_SIZE (2 * 8 * N) //In Bytes
+#define OPTIMIZATION_2
+
+#endif
